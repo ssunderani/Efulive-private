2ef61293b17e792ea1a2e23e0fff50e780b9fd29
ahkalwar <aizazkalwar46@gmail.com>
2021-07-19 12:24:39 +0500

    login btn color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
 
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn" style="background: violet; padding: 3px 40px;">
+                            <button type="submit" class="btn btn-primary" style="padding: 3px 40px;">
                                 {{ __('Login') }}
                             </button>
 
